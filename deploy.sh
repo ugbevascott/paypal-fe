399c9186449cd968ec4f4f3587fa148997fd80cc
Scott Ugbeva <ugbeva@gmail.com>
2024-05-23 21:12:12 +0100

    deployed updated

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,8 @@ But Boca took it out when they renewed him through to 2028 this month after it b
 
 A bidding war between Prem clubs would suit Boca down to the ground and journalist Fafi Perez has claimed Chelsea are Anselmino’s latest suitors. 
 
-The reporter told DSports Radio listeners that United remain the most interested club in the uncapped teenager.
+The reporter told DSports Radio listeners that United remain the most interested club in the uncapped teenager.Ex-Leicester boss Brendan Rodgers has revealed that Europa League final hero Ademola Lookman could have signed for the East Midlanders had they activated a £14m option after a previous loan spell.
+
+Speaking to talkSPORT, he said: “What a professional he is, how he looks after himself, the extra work he does in training. Those goals that he scored, he practises those after training. He'd always do extra finishing. His professionalism was unbelievable and the only sad part for us was at the end of that season, we had a clause where we could have bought him for £14 million.
+
+“But if you think back to that summer we couldn't sign any players and then one of my guys that I worked closely with here at Celtic and at Leicester was a guy called Lee Congerton and Lee was by then the general manager of Atalanta. When he knew that we weren't able to take him, he took him then out to Atalanta and he's done absolutely brilliant there."
