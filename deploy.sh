cad42e49163568f5be3fdbef1508bb9bf2bae9db
Scott Ugbeva <ugbeva@gmail.com>
2024-05-23 21:04:41 +0100

    deploy created

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 Application is ready to be deployed to the development environment
-development must be reviewed
-by managers
+development must be reviewed by managers.
+They updated his terms to include a 20m US$ release (£15.72m) release clause in January.
+
+But Boca took it out when they renewed him through to 2028 this month after it became obvious the amount was not high enough to deter offers.
+
+A bidding war between Prem clubs would suit Boca down to the ground and journalist Fafi Perez has claimed Chelsea are Anselmino’s latest suitors. 
+
+The reporter told DSports Radio listeners that United remain the most interested club in the uncapped teenager.
